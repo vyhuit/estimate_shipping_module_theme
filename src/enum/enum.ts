0beeae3414716a757fdfc532fe25fc506ef04615
Vo Yen Hoang <vyhuit.m@gmail.com>
2022-11-23 15:44:09 +0700

    Hoang improve UI

---
+++ src/enum/enum.ts
@@ -0,0 +1,20 @@
+const HTMLTAG = {
+  DIV: "div",
+  P: "p",
+  I: "i",
+  SPAN: "span",
+  UL: "ul",
+  LI: "li"
+};
+const CONSTANT = {
+  LABEL_EST: "Estimate arrival:",
+  LABEL_NOTICE_1: "The above time frame is only applied for orders to the US with standard shipping methods",
+  LABEL_NOTICE_2: "International orders: It may take 2-5 days longer due to the customs clearance process.",
+
+  CLASSNAME_ESTIMATE_SHIPPING_INIT: "eb-estimate-shipping"
+};
+
+export {
+  HTMLTAG,
+  CONSTANT
+};
--- src/estimate-shipping/estimate-shipping.js
+++ src/estimate-shipping/estimate-shipping.js
@@ -1,99 +1,91 @@
-console.log("Zoooo");
+import {HTMLTAG, CONSTANT} from "../enum/enum"
+
 let getData = () => {
-  let productType = window.product.type == "" ? "nonType" : window.product.type;
-  //call api for get the estimated time with above productType
-  
+  let productType = window.product.type == ""
+    ? "nonType"
+    : window.product.type;
+  return productType;
+};
+
+let createElementWithClassname = (tag, ...className) => {
+  let element = document.createElement(tag);
+  element.classList.add(...className);
+  return element;
+}
+
+let stepperItemGener = (icon, time, label) => {
+  const CLASSNAME_STEPPER_ITEM = "stepper-item";
+  const CLASSNAME_STEPPER_CONTENT = "stepper-content";
+  const CLASSNAME_STEPPER_ICON = "stepper-icon";
+  const CLASSNAME_TIME_VALUE = "time";
+  const CLASSNAME_TIME_LABEL = "time-label";
+
+  let stepperItem = createElementWithClassname(HTMLTAG.DIV, CLASSNAME_STEPPER_ITEM);
+  let stepperContentWrapper = createElementWithClassname(HTMLTAG.DIV, CLASSNAME_STEPPER_CONTENT);
+  let stepperIconWrapper = createElementWithClassname(HTMLTAG.DIV, CLASSNAME_STEPPER_ICON);
+  let timeElement = createElementWithClassname(HTMLTAG.P, CLASSNAME_TIME_VALUE);
+  let labelElement = createElementWithClassname(HTMLTAG.P, CLASSNAME_TIME_LABEL);
+
+  timeElement.append(time);
+  labelElement.append(label);
+  stepperIconWrapper.append(icon);
+  stepperContentWrapper.append(timeElement, labelElement);
+  stepperItem.append(stepperIconWrapper, stepperContentWrapper);
+
+  return stepperItem;
 };
 
 class EstimateShipping {
   constructor() {
     const data = getData();
-    this.wrapper = document.createElement('div');
-    this.wrapper.classList.add("estimate-title");
+    this.element = this.initElement(data);
+  }
+  initElement(data) {
+    let wrapper = createElementWithClassname(HTMLTAG.DIV, "estimate-title");
+    let deliveryDay = "Nov 21 - Dec 12"; // get from data.abc
 
-// create hoverText Day from api data
-    let deliveryDay = "Nov 21 - Dec 12";
+    let carIcon = createElementWithClassname(HTMLTAG.I, "fa", "fa-truck", "label-icon");
+    let orderIcon = createElementWithClassname(HTMLTAG.I, "fa", "fa-handshake-o");
+    let deliveryIcon = createElementWithClassname(HTMLTAG.I, "fa", "fa-gift");
+    let shippingIcon = createElementWithClassname(HTMLTAG.I, "fa", "fa-truck");
 
-// constant label
-    const EST_LABEL = "Estimate arrival:"
+    wrapper.append(carIcon);
+    wrapper.append(CONSTANT.LABEL_EST);
 
-// create icons
-    let carIcon = document.createElement("i");
-    carIcon.classList.add("fa", "fa-truck", "label-icon");
-    let orderIcon = document.createElement("i");
-    orderIcon.classList.add("fa", "fa-handshake-o");
-    let deliveryIcon = document.createElement("i");
-    deliveryIcon.classList.add("fa", "fa-gift");
-    let shippingIcon = document.createElement("i");
-    shippingIcon.classList.add("fa", "fa-truck");
-
-// add car icon and constant label first
-    this.wrapper.append(carIcon);
-    this.wrapper.append(EST_LABEL);
-
-// create hover text element and popup inside
-    let hoverText = document.createElement("span");
-    hoverText.classList.add("hover-text");
+    let hoverText = createElementWithClassname(HTMLTAG.SPAN, "hover-text");
     hoverText.append(deliveryDay)
 
-    let popup = document.createElement("div");
-    popup.classList.add("tooltip-text", "est-card")
-    let stepper = document.createElement("div");
-    stepper.classList.add("shipping-stepper");
+    let popup = createElementWithClassname(HTMLTAG.DIV, "tooltip-text", "est-card");
+    let stepper = createElementWithClassname(HTMLTAG.DIV, "shipping-stepper");
 
-    let stepperItemGener = (icon, time, label) => {
-      let stepperItem = document.createElement("div");
-      stepperItem.classList.add("stepper-item");
-
-      let stepperContentWrapper = document.createElement("div");
-      stepperContentWrapper.classList.add("stepper-content");
-
-      let stepperIconWrapper = document.createElement("div");
-      stepperIconWrapper.classList.add("stepper-icon");
-
-      let timeElement = document.createElement("p");
-      let labelElement = document.createElement("p");
-
-      timeElement.classList.add("time");
-      labelElement.classList.add("time-label");
-      timeElement.append(time);
-      labelElement.append(label);
-
-      stepperIconWrapper.append(icon);
-      stepperContentWrapper.append(timeElement, labelElement);
-      stepperItem.append(stepperIconWrapper, stepperContentWrapper);
-
-      return stepperItem;
-    };
     let orderStepItem = stepperItemGener(orderIcon, "Dec 12 - 15", "Order Placing");
     let shippingStepItem = stepperItemGener(shippingIcon, "Dec 16 - 18", "Order Shipping");
     let deliveryStepItem = stepperItemGener(deliveryIcon, "Dec 18 - 20", "Order delivering");
     stepper.append(orderStepItem, shippingStepItem, deliveryStepItem);
     popup.append(stepper);
 
-    let noticeWrapper = document.createElement("div");
-    noticeWrapper.classList.add("est-shipping-notice");
-    let ul = document.createElement("ul");
-    let li1 = document.createElement("li");
-    li1.innerText = "The above time frame is only applied for orders to the US with standard shipping methods.";
-    let li2 = document.createElement("li");
-    li2.innerText = "International orders: It may take 2-5 days longer due to the customs clearance process.";
+    let noticeWrapper = createElementWithClassname(HTMLTAG.DIV, "est-shipping-notice");
+    let ul = createElementWithClassname(HTMLTAG.SPAN);
+    let li1 = createElementWithClassname(HTMLTAG.LI);
+    li1.innerText = CONSTANT.LABEL_NOTICE_1;
+    let li2 = createElementWithClassname(HTMLTAG.LI);
+    li2.innerText = CONSTANT.LABEL_NOTICE_2;
     ul.append(li1, li2);
     noticeWrapper.append(ul);
 
     popup.append(noticeWrapper);
     hoverText.append(popup);
-    this.wrapper.append(hoverText);
-    console.log(this.wrapper);
-    return;
+    wrapper.append(hoverText);
+    return wrapper;
   }
 
   getEls() {
-    return {wrapper: this.wrapper}
+    return {element: this.element}
   }
 }
 
-const estimateShipping = new EstimateShipping()
-
-const estimateShippingEl = document.querySelector('.eb-estimate-shipping');
-estimateShippingEl.appendChild(estimateShipping.getEls().wrapper)
+const ES = new EstimateShipping()
+const estimateShippingEl = document.querySelector(`.${
+  CONSTANT.CLASSNAME_ESTIMATE_SHIPPING_INIT
+}`);
+estimateShippingEl.appendChild(ES.getEls().element)
